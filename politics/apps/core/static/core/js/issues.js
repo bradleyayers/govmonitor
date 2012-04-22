69efac1d44bbc1a115bc758739251546407dfe7f
Chris Doble <chris@chrisdoble.com>
2012-04-22 18:48:40 +1000

    Fix the Google Charts JavaScript error (fixes #30).

--- politics/apps/core/static/core/js/issues.js
+++ politics/apps/core/static/core/js/issues.js
@@ -127,45 +127,47 @@ jQuery(function($) {
 
     $("form.issue #name").focus();
 
-    // Draw the stance distribution chart. Google charts is initialised in its
-    // own script tag on the page. Chart data is in a global variable `stances`.
-    google.setOnLoadCallback(function() {
-        var data = new google.visualization.DataTable();
-        data.addColumn("string", "Stance");
-        data.addColumn("number", "Parties");
+    if ($("html").hasClass("issue")) {
+        // Draw the stance distribution chart. Google charts is initialised in its
+        // own script tag on the page. Chart data is in a global variable `stances`.
+        google.setOnLoadCallback(function() {
+            var data = new google.visualization.DataTable();
+            data.addColumn("string", "Stance");
+            data.addColumn("number", "Parties");
 
-        $.each(stances, function(i, stance) {
-            data.addRow(stance);
+            $.each(stances, function(i, stance) {
+                data.addRow(stance);
+            });
+
+            var options = {
+                "backgroundColor": "#F9F4F3",
+                "chartArea": {
+                    "height": "100%",
+                    "left": 0,
+                    "top": 0,
+                    "width": "100%"
+                },
+                "colors": ["#FFA6A6", "#B1F0AF", "#FFED93", "#E8E8E8"],
+                "enableInteractivity": false,
+                "height": 130,
+                "legend": {
+                    "position": "right",
+                    "textStyle": {
+                        "color": "#333333",
+                        "fontName": "Helvetica Neue",
+                        "fontSize": 12
+                    }
+                },
+                "pieSliceBorderColor": "#F9F4F3",
+                "pieSliceText": "none",
+                "tooltip": {
+                    "trigger": "none"
+                },
+                "width": 220
+            };
+
+            var chart = new google.visualization.PieChart($("#stances-chart")[0]);
+            chart.draw(data, options);
         });
-
-        var options = {
-            "backgroundColor": "#F9F4F3",
-            "chartArea": {
-                "height": "100%",
-                "left": 0,
-                "top": 0,
-                "width": "100%"
-            },
-            "colors": ["#FFA6A6", "#B1F0AF", "#FFED93", "#E8E8E8"],
-            "enableInteractivity": false,
-            "height": 130,
-            "legend": {
-                "position": "right",
-                "textStyle": {
-                    "color": "#333333",
-                    "fontName": "Helvetica Neue",
-                    "fontSize": 12
-                }
-            },
-            "pieSliceBorderColor": "#F9F4F3",
-            "pieSliceText": "none",
-            "tooltip": {
-                "trigger": "none"
-            },
-            "width": 220
-        };
-
-        var chart = new google.visualization.PieChart($("#stances-chart")[0]);
-        chart.draw(data, options);
-    });
+    }
 });
