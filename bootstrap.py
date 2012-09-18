ebb1ecaea35775a85a9db631cd6bb169e7a41b69
Bradley Ayers <bradley.ayers@gmail.com>
2012-09-18 19:55:34 +1000

    Re-over-engineer bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -2,13 +2,14 @@
 # coding=utf-8
 import os
 import os.path
-import subprocess
+from subprocess import CalledProcessError, check_call, check_output, STDOUT
 import sys
 
 
 # The root directory of the project and the project's name.
 ROOT = os.path.dirname(os.path.abspath(__file__))
 NAME = os.path.basename(ROOT)
+VIRTUALENV_PATH = os.path.join(ROOT, ".env")
 
 
 # Output colours. We need to define these ourselves because we don't want to
@@ -16,7 +17,7 @@ NAME = os.path.basename(ROOT)
 def _wrap_colour(code):
     """Returns a function that wraps text in the given ANSI escape code."""
     def inner(text):
-       return "\033[1;%sm%s\033[0m" % (code, text)
+        return "\033[1;%sm%s\033[0m" % (code, text)
 
     return inner
 
@@ -25,11 +26,11 @@ red = _wrap_colour("31")
 green = _wrap_colour("32")
 
 
-def check_call(arguments):
+def silent_call(arguments):
     """A silent version of subprocess's ``check_call()``."""
     try:
         devnull = open(os.devnull, "w")
-        subprocess.check_call(arguments, stderr=devnull, stdout=devnull)
+        check_call(arguments, stderr=devnull, stdout=devnull)
     finally:
         devnull.close()
 
@@ -39,8 +40,8 @@ def create_virtualenv():
     print "> Creating a virtualenv..."
 
     try:
-        check_call(["virtualenv", "--no-site-packages", get_virtualenv_path()])
-    except subprocess.CalledProcessError:
+        silent_call(["virtualenv", "--no-site-packages", VIRTUALENV_PATH])
+    except CalledProcessError:
         print >> sys.stderr, red("Couldn't create a virtualenv.")
         sys.exit(1)
 
@@ -63,32 +64,35 @@ def executable_exists(executable):
     try:
         # If no executables could be found, which's returned code will be 1,
         # causing a subprocess.CalledProcessError exception to be raised.
-        check_call(["which", executable])
+        silent_call(["which", executable])
         return True
-    except subprocess.CalledProcessError:
+    except CalledProcessError:
         return False
 
 
-def get_virtualenv_path():
-    """Returns the path to the project's virtualenv."""
-    return os.path.join(ROOT, "environment")
+def investigate_and_report(error):
+    """Print a message explaining the exception.
+
+    :type error: CalledProcessError object with ``output`` attribute
+    """
+    guesses = {"libmemcached": "'libmemcached/memcached.h' file not found"}
+    for libary, signature in guesses.items():
+        if signature in error.output:
+            reason = "'%s' is required but not installed." % libary
+            print >> sys.stderr, red(reason)
 
 
 def install_requirements():
     """Installs the project's requirements to the virtualenv."""
     print "> Installing requirements..."
 
-    # Do we have a local cache?
-    if os.path.exists(os.path.join(ROOT, "requirements_cache")):
-        print "Some requirements are cached locally; we'll use those."
-
     try:
         # Install the packages listed in requirements.txt into the project's
         # virtualenv, opting to use those in the local cache if available.
-        check_call(["pip", "install", "--download-cache", "requirements_cache",
-                    "-E", get_virtualenv_path(), "-r", "requirements.txt"])
-    except subprocess.CalledProcessError:
-        print >> sys.stderr, red("Couldn't install the requirements.")
+        pip = os.path.join(VIRTUALENV_PATH, "bin", "pip")
+        check_output([pip, "install", "-r", "requirements.txt"], stderr=STDOUT)
+    except CalledProcessError as e:
+        investigate_and_report(e)
         sys.exit(1)
 
 
@@ -102,10 +106,14 @@ def main():
         print >> sys.stderr, red("pip must be installed.")
         sys.exit(1)
 
+    # Honor PIP_DOWNLOAD_CACHE environment variable.
+    os.environ.setdefault("PIP_DOWNLOAD_CACHE",
+                          os.path.join(ROOT, "requirements_cache"))
+
     create_virtualenv()
     install_requirements()
 
-    print green("Done! Now just run `source environment/bin/activate`.")
+    print green("Done! Now just run `. .env/bin/activate`.")
 
 
 if __name__ == "__main__":
